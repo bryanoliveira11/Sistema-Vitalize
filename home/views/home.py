ec903236a431a9101b3fbe89a7282ae9cc6739ad
bryan <bryan.oliveira12345@gmail.com>
2024-05-27 14:46:13 -0300

    Sidebar

--- home/views/home.py
+++ home/views/home.py
@@ -4,4 +4,4 @@ from django.views.generic import View
 
 class HomePage(View):
     def get(self, *args, **kwargs):
-        return render(self.request, 'home/pages/home.html', {'title': 'Home'})
+        return render(self.request, 'home/pages/home.html', {})
