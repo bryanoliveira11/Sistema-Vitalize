46608ac2e0324caf1e6dc3e45dadde857388079b
bryan <bryan.oliveira12345@gmail.com>
2024-06-06 11:53:36 -0300

    Remoção da sidebar

--- base_static/global/js/build/scripts.js
+++ base_static/global/js/build/scripts.js
@@ -9,29 +9,3 @@ const addClassList = (element, value) => {
 const removeClassList = (element, value) => {
   if (element) element.classList.remove(value);
 }
-
-(() => {
-  const hamburguerMenu = document.querySelector('#menu-hamburguer');
-  const closeMenuIcon = document.querySelector('#close-menu');
-  const sidebar = document.querySelector('.sidebar');
-  const fadeInClass = 'fade-in';
-
-  if (!hamburguerMenu || !closeMenuIcon || !sidebar) return;
-
-  const showMenu = () => {
-    addClassList(sidebar, fadeInClass);
-    changeDisplayStyle(sidebar, 'flex');
-    changeDisplayStyle(hamburguerMenu, 'none');
-    changeDisplayStyle(closeMenuIcon, 'block');
-  };
-
-  const closeMenu = () => {
-    removeClassList(sidebar, fadeInClass);
-    changeDisplayStyle(sidebar, 'none');
-    changeDisplayStyle(hamburguerMenu, 'block');
-    changeDisplayStyle(closeMenuIcon, 'none');
-  }
-
-  hamburguerMenu.addEventListener('click', showMenu);
-  closeMenuIcon.addEventListener('click', closeMenu);
-})();
