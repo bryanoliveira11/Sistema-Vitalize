5de1a60c3c406deda6db1db8d2f4ba425473c426
bryan <bryan.oliveira12345@gmail.com>
2024-08-13 19:09:12 -0300

    Remover mensagens da tela

--- base_static/global/js/build/scripts.js
+++ base_static/global/js/build/scripts.js
@@ -9,3 +9,35 @@ const addClassList = (element, value) => {
 const removeClassList = (element, value) => {
   if (element) element.classList.remove(value);
 }
+
+function ChangeDisplayAnimationEnd(element) {
+  element.addEventListener(
+    'animationend',
+    () => {
+      element.style.display = 'none';
+    },
+    { once: true },
+  );
+}
+
+class DismissFlashMessages {
+  constructor() {
+    this.dismissMessageBtns = Array.from(
+      document.querySelectorAll('.dismiss-flash-message'),
+    );
+  }
+  init() {
+    if (!this.dismissMessageBtns) return;
+    for (const btn of this.dismissMessageBtns) {
+      btn.addEventListener('click', () => {
+        const parentElement = btn.parentElement;
+        if (parentElement.classList.contains('message')) {
+          parentElement.classList.add('hide-message');
+          ChangeDisplayAnimationEnd(parentElement);
+        }
+      });
+    }
+  }
+}
+
+new DismissFlashMessages().init();
--- home/views/home.py
+++ home/views/home.py
@@ -8,11 +8,6 @@ class HomePage(View):
         title = 'Vitrine'
         subtitle = 'de Produtos'
 
-        messages.success(self.request, 'mensagem de teste')
-        messages.error(self.request, 'mensagem de teste')
-        messages.info(self.request, 'mensagem de teste')
-        messages.warning(self.request, 'mensagem de teste')
-
         return render(
             self.request,
             'home/pages/home.html',
