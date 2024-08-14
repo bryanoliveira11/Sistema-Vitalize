25142a11070472716dcf58da911b984d97060299
bryan <bryan.oliveira12345@gmail.com>
2024-08-14 11:46:11 -0300

    Javascript para checar e informar se o usuário está atendendo os requisitos de senha em tempo real.

--- base_static/global/js/build/scripts.js
+++ base_static/global/js/build/scripts.js
@@ -1,20 +1,20 @@
 const changeDisplayStyle = (element, display) => {
   if (element) element.style.display = display;
-}
+};
 
 const addClassList = (element, value) => {
   if (element) element.classList.add(value);
-}
+};
 
 const removeClassList = (element, value) => {
   if (element) element.classList.remove(value);
-}
+};
 
 function ChangeDisplayAnimationEnd(element) {
   element.addEventListener(
     'animationend',
     () => {
-      element.style.display = 'none';
+      changeDisplayStyle(element, 'none');
     },
     { once: true },
   );
@@ -40,4 +40,69 @@ class DismissFlashMessages {
   }
 }
 
+class HandlePasswordTipsStyles {
+  constructor() {
+    this.passwordField = document.querySelector('#id_password');
+    this.helpTexts = Array.from(
+      document.querySelectorAll('.helptext-p.password'),
+    );
+  }
+
+  init() {
+    if (!this.passwordField || !this.helpTexts) return;
+    this.passwordField.addEventListener('keyup', () => this.handleStyle());
+  }
+
+  handleStyle() {
+    const conditions = [
+      !(this.passwordField.value.length < 8),
+      /^(?=.*[A-Z])/.test(this.passwordField.value),
+      /^(?=.*[a-z])/.test(this.passwordField.value),
+      /^(?=.*\d)/.test(this.passwordField.value),
+    ];
+    const errorColor = '#dc3545';
+    const successColor = '#28a745';
+
+    conditions.forEach((condition, index) => {
+      const color = condition ? successColor : errorColor;
+      this.changeHelpTextColor(index, color);
+
+      if (!condition) {
+        this.addIcon(index, 'fa-circle-exclamation', errorColor);
+        this.removeIcon(index, 'fa-check');
+      } else {
+        this.addIcon(index, 'fa-check', successColor);
+        this.removeIcon(index, 'fa-circle-exclamation');
+      }
+    });
+  }
+
+  changeHelpTextColor(index, color) {
+    if (!this.helpTexts[index]) return;
+    this.helpTexts[index].style.color = color;
+  }
+
+  addIcon(index, iconClass, color) {
+    if (!this.helpTexts[index]) return;
+
+    let icon = this.helpTexts[index].querySelector(`.${iconClass}`);
+    if (!icon) {
+      icon = document.createElement('i');
+      icon.className = `fa-solid ${iconClass}`;
+      icon.style.marginLeft = '8px';
+      icon.style.color = color;
+      this.helpTexts[index].appendChild(icon);
+    }
+  }
+
+  removeIcon(index, iconClass) {
+    if (!this.helpTexts[index]) return;
+    const icon = this.helpTexts[index].querySelector(`.${iconClass}`);
+    if (icon) {
+      this.helpTexts[index].removeChild(icon);
+    }
+  }
+}
+
 new DismissFlashMessages().init();
+new HandlePasswordTipsStyles().init();
