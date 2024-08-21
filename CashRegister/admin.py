d767e31149970eaff261a1aacc277e48ccb997e6
bryan <bryan.oliveira12345@gmail.com>
2024-08-21 19:09:48 -0300

    ajustes admin CashRegister

--- CashRegister/admin.py
+++ CashRegister/admin.py
@@ -7,8 +7,8 @@ from CashRegister.models import CashRegister, CashOut
 class AdminVitalizeCashRegister(admin.ModelAdmin):
     list_display = 'id', 'cash', 'is_open', 'open_date', 'close_date',
     list_display_links = 'id',
-    list_editable = 'is_open', 'close_date', 'value',
+    list_editable = 'is_open',
     # search_fields = 'product_name',
     ordering = '-id',
-    list_filter = 'product_category',
+    list_filter = 'open_date', 'close_date',
     list_per_page = 20
