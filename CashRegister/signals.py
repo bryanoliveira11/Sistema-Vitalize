03b5b16d7fdfb366cbaffec437cab766ff66ac6d
bryan <bryan.oliveira12345@gmail.com>
2024-09-24 14:12:55 -0300

    Otimizando o update_total_price do caixa para receber o valor a ser inserido ou retirado do caixa, assim evitando de recalcular o valor toda vez.

--- CashRegister/signals.py
+++ CashRegister/signals.py
@@ -2,10 +2,19 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
 from CashRegister.models import CashRegister
+from Sales.models import Sales
 
 
 @receiver(m2m_changed, sender=CashRegister.sales.through)
-def cashregister_update_price(instance, action, **kwargs):
-    if action in ['post_add', 'post_remove', 'post_clear']:
-        if instance.is_open:
-            instance.update_total_price()
+def cashregister_update_price(instance, action, pk_set, **kwargs):
+    if not instance.is_open:
+        return
+
+    if action == 'post_add':
+        for sale_pk in pk_set:
+            sale = Sales.objects.get(pk=sale_pk)
+            instance.update_total_price(sale.total_price, operation='add')
+    elif action == 'post_remove':
+        for sale_pk in pk_set:
+            sale = Sales.objects.get(pk=sale_pk)
+            instance.update_total_price(sale.total_price, operation='remove')
