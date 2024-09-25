c5e0812b58c78847155e54238a1423096547fcab
bryan <bryan.oliveira12345@gmail.com>
2024-09-25 11:11:24 -0300

    Inclusão de modals para confirmar a abertura e fechamento do caixa. Ajuste em problema com timezones na hora de validar se há algum caixa aberto.

--- utils/cashregister_utils.py
+++ utils/cashregister_utils.py
@@ -1,18 +1,24 @@
+import pytz
 from django.utils import timezone
 
 from CashRegister.models import CashRegister
 
 
 def get_today_cashregister():
-    time_start = timezone.now().replace(
-        hour=0, minute=0, second=0, microsecond=0
-    )
-    time_end = timezone.now().replace(
-        hour=23, minute=59, second=59, microsecond=999999
-    )
+    local_tz = pytz.timezone('America/Sao_Paulo')
+    now_local = timezone.now().astimezone(local_tz)
 
-    cashregister = CashRegister.objects.filter(
-        is_open=True, open_date__range=(time_start, time_end),
-    ).prefetch_related('sales', 'cash_out').first()
+    time_start_local = now_local.replace(
+        hour=0, minute=0, second=0, microsecond=0)
+    time_end_local = now_local.replace(
+        hour=23, minute=59, second=59, microsecond=999999)
 
-    return cashregister or None
+    time_start_utc = time_start_local.astimezone(pytz.UTC)
+    time_end_utc = time_end_local.astimezone(pytz.UTC)
+
+    cashregisters = CashRegister.objects.filter(
+        is_open=True,
+        open_date__range=(time_start_utc, time_end_utc)
+    ).prefetch_related('sales', 'cash_out')
+
+    return cashregisters.first() or None
